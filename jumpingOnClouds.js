e21272198e982f7637828d266c02e2c1de9e2975
RudolfGrill <grillrudolf@gmail.com>
2018-12-13 21:46:14 +0100

    jumping on clouds is fun

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -4,49 +4,35 @@ const c1 = [0, 0, 1, 0, 0, 1, 0]; //4
 const c2 = [0, 0, 0, 0, 1, 0]; //3
 const c3 = [0, 0, 1, 0, 0, 0, 0, 1, 0, 0]; //6
 const c4 = [0, 1, 0]; //1
-
-
-
-
+const c5 = [0, 0] //1
 
 function jumpingOnClouds(c) {
-  console.log(c, 'eredeti')
-  console.log('c.length: ', c.length)
   let steps = 0;
+  if (c[c.length - 3] === 1) {
+    steps++;
+  }
+  if (c.length === 2) {
+     steps++;
+  }
   for (let i = 0; i < c.length; i++) {
-    if (c[i] === 1 && c[i + 1] === 0) {
-      console.log(c[i], c[i + 1])
-      console.log(c, 'egy es nulla vágás előtt')
-      steps++
-      c.splice(0, 2)
-      console.log(c, 'egy es nulla')
-
+    if (c[i] === 0 && c[i + 1] === 0 && c[i + 2] === 0 ||
+      c[i] === 0 && c[i + 1] === 1 && c[i + 2] === 0) {
+      steps++;
+      c.splice(0, 2);
     } else if (c[i] === 0 && c[i + 1] === 0 && c[i + 2] === 1) {
-      console.log(c[i], c[i + 1])
-      console.log(c, 'nulla es nulla vágás előtt')
-      steps++
-      c.splice(0, 1)
-      console.log(c, 'dupla nulla')
-
-    } else if (c[i] === 0 && c[i + 1] === 1) {
-      console.log(c[i], c[i + 1])
-      console.log(c, 'nulla es nulla vágás előtt')
-      steps++
-      c.splice(0, 2)
-      console.log(c, 'dupla nulla')
+      steps++;
+      c.splice(0, 1);
 
     } else if (c[i] === 0) {
-      console.log(c[i])
-      console.log(c, 'nulla vágás előtt')
-      c.splice(0, 1)
-      console.log(c, 'nulla')
+      c.splice(0, 1);
     }
     i--;
   }
   return steps;
 };
 
-//console.log(jumpingOnClouds(c1));
-//console.log(jumpingOnClouds(c2));
-console.log(jumpingOnClouds(c3));
-//console.log(jumpingOnClouds(c4));
+console.log(jumpingOnClouds(c1), '4');
+console.log(jumpingOnClouds(c2), '3');
+console.log(jumpingOnClouds(c3), '6');
+console.log(jumpingOnClouds(c4), '1');
+console.log(jumpingOnClouds(c5), '1');
