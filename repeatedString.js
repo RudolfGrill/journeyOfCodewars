ba132ed4b1badc8f3707f382ac478970d5131ce1
RudolfGrill <grillrudolf@gmail.com>
2018-12-19 21:20:42 +0100

    repeated String done

--- repeatedString.js
+++ repeatedString.js
@@ -1,1 +1,21 @@
 'use strict'
+
+let s1 = 'abcaca';
+let n1 = 10;
+
+let s2 = 'aba';
+let n2 = 10;  
+
+
+function repeatedString(s, n) {
+  let aCount = (str) => str.split('')
+    .filter(e => e === 'a')
+    .length;
+  let m = Math.floor(n / s.length)
+  let rest = n - (s.length * m)
+  return m * aCount(s) + aCount(s.substr(0, rest))
+
+}
+
+console.log(repeatedString(s1, n1));
+console.log(repeatedString(s2, n2));
