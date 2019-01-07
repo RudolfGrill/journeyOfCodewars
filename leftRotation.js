e5c9fb1ce7a74268fbd28c9466cbfb421899df97
RudolfGrill <grillrudolf@gmail.com>
2019-01-07 15:03:49 +0100

    feat(leftRotation) give back the right order

--- leftRotation.js
+++ leftRotation.js
@@ -1,7 +1,15 @@
 'use strict'
-let a = 5;
-let d = 
+//A left rotation operation on an array shifts each of the array's elements  unit to the left.
+
+let a = [1, 2, 3, 4, 5];
+let d = 4;
+
 function rotLeft(a, d) {
-
-
+  for (let i = 0; i < d; i++) {
+    a.push(a[0])
+    a.splice(0, 1);
+  }
+  return a
 }
+
+console.log(rotLeft(a, d));
